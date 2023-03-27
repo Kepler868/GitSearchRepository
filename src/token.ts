ec48233a8a6df40cc5d60d4c0ff8e861715b215f
Kepler868 <105875420+Kepler868@users.noreply.github.com>
2023-03-27 14:58:55 +0300

    Update token.ts

--- src/token.ts
+++ src/token.ts
@@ -1,5 +1,5 @@
 // Type here your Github Private Access Token to see a list of your repositories in the app, or my repositories will be displayed 
-export const API_TOKEN = "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";
+export const API_TOKEN = "";
 
 export const GITHUB_API = `https://api.github.com/graphql`;
 
